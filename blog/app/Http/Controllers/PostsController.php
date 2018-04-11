8e8505c9b9311ad85ed82351d9d2df20a5bd9ee4
Tirsameer <trisameer@gmail.com>
2018-04-11 11:33:21 +0530

    Declared a rest api as controller var

--- blog/app/Http/Controllers/PostsController.php
+++ blog/app/Http/Controllers/PostsController.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Config;
 
 class PostsController extends Controller
 {
+    // Declare private variable
+    private $rest_url;
+    
     /**
      * Create a new controller instance.
      *
@@ -13,19 +16,18 @@ class PostsController extends Controller
      */
     public function __construct()
     {
-        //
+        $this->rest_url = config('constants.rest-api-url');
     }
     // Listing all posts
     public function viewposts() {
-        $rest_url = config('constants.rest-api-url');
-        $url = $rest_url . "/wp-json/wp/v2/posts/";
+        $url = $this->rest_url . "/wp-json/wp/v2/posts/";
         $json = json_decode(file_get_contents($url), true);
         return view('posts', ['posts'=> $json]);
     }
     // Retrieve single post
     public function singlepost(Request $request, $post_id) {
-        $rest_url = config('constants.rest-api-url');
-        $url = $rest_url . "/wp-json/wp/v2/posts/".$post_id;
+        
+        $url = $this->rest_url . "/wp-json/wp/v2/posts/".$post_id;
         $json = json_decode(file_get_contents($url), true);
         return view('post', ['post'=> $json]);
     }
